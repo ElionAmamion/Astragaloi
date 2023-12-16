2ff8e893857cc5af13d85ce7a16afde0e85d5112
ElionAmamion <151955833+ElionAmamion@users.noreply.github.com>
2023-12-16 21:02:39 +0100

    carg dependencies made

---
+++ main/target/debug/deps/main.d
@@ -0,0 +1,5 @@
+C:\Users\tylfr.KORILAKKUMA\Documents\GitHub\Astragaloi\main\target\debug\deps\main.exe: src\main.rs
+
+C:\Users\tylfr.KORILAKKUMA\Documents\GitHub\Astragaloi\main\target\debug\deps\main.d: src\main.rs
+
+src\main.rs:
---
+++ main/target/debug/main.d
@@ -0,0 +1,1 @@
+C:\Users\tylfr.KORILAKKUMA\Documents\GitHub\Astragaloi\main\target\debug\main.exe: C:\Users\tylfr.KORILAKKUMA\Documents\GitHub\Astragaloi\main\src\main.rs
