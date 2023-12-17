8b25e8eafe68247e8d25bf0dcb4ef19298f1f7b8
ElionAmamion <151955833+ElionAmamion@users.noreply.github.com>
2023-12-17 08:40:43 +0100

    changed file directory and more

---
+++ main/target/debug/deps/main-f73cb097b8a9e016.d
@@ -0,0 +1,5 @@
+C:\Users\tylfr.KORILAKKUMA\Documents\GitHub\Astragaloi\main\target\debug\deps\libmain-f73cb097b8a9e016.rmeta: src\main.rs
+
+C:\Users\tylfr.KORILAKKUMA\Documents\GitHub\Astragaloi\main\target\debug\deps\main-f73cb097b8a9e016.d: src\main.rs
+
+src\main.rs:
