b9f6d100360d2ee26c8704e2ef4afaac5dffcc2a
ElionAmamion <151955833+ElionAmamion@users.noreply.github.com>
2023-12-17 17:59:16 +0100

    just to secure data

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,6 @@
+use std::io;
+
 fn main() {
-    let mut x = 4;
-    println!("x is: {}",x);
-    x = 5;
-    println!("x is: {}",x);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read line");
 }
