66ee1a6dcf1eae2d58355cf5240c355f168ac15a
ElionAmamion <151955833+ElionAmamion@users.noreply.github.com>
2023-12-17 21:39:45 +0100

    mono additive calculator

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,8 +1,13 @@
-//use std::io;
+use std::io;
 
 fn main() {
-    let x = 12;
-    let y = 10;
-    let z = x as f64 / y as f64;
-    println!("{}", z);
+    let mut input1 = String::new();
+    io::stdin().read_line(&mut input1).expect("couldn't read line");
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("couldn't read line");
+
+    let int_input1: f64 =input1.trim().parse().unwrap();
+    let int_input2: f64 =input2.trim().parse().unwrap();
+
+    println!("{}", int_input1 + int_input2);
 }
