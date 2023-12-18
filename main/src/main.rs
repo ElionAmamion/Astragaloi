2417aded09d962747c16ff0f7234d4faac47595b
ElionAmamion <151955833+ElionAmamion@users.noreply.github.com>
2023-12-18 17:54:35 +0100

    end of session

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,13 +1,5 @@
-use std::io;
-
 fn main() {
-    let mut input1 = String::new();
-    io::stdin().read_line(&mut input1).expect("couldn't read line");
-    let mut input2 = String::new();
-    io::stdin().read_line(&mut input2).expect("couldn't read line");
-
-    let int_input1: f64 =input1.trim().parse().unwrap();
-    let int_input2: f64 =input2.trim().parse().unwrap();
-
-    println!("{}", int_input1 + int_input2);
+    if "main" == "main" {
+        println!("Main = Main");
+    };
 }
