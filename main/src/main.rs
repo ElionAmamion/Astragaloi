15c2f1b7e2389680deaac1b3369d90a9e7179aed
ElionAmamion <151955833+ElionAmamion@users.noreply.github.com>
2024-01-19 22:04:10 +0100

    bord is now working <> werden Variablen

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,25 +1,35 @@
-use std::process::Command;
+//use std::process::Command; für den anderen, großen Komentar
+use std::io;
 
 fn main() {
-
-    let output = Command::new("<link>dice.exe</link>")
+    /* Versuchdice.exe zu importieren
+    let output = Command::new("dice.exe")
         .output()
-        .expect("Failed to run the executable");
+        .expect("Failed to execute command");
 
-    println!("Output: {:?}", output);
+    println!("Output: {:?}", output);*/
 
-    /*println!("Astragaloi");
-    println!("##########");
-    println!("Du hast gewürfelt: <dice>");
-    println!("Dein Gegner hat folgende Würfel:");
-    println!("   [ -- A -- ] [ -- B -- ] [ -- C -- ]");
-    println!(" 3 [ <p2-a3> ] [ <p2-b3> ] [ <p2-c3> ]");
-    println!(" 2 [ <p2-a2> ] [ <p2-b2> ] [ <p2-c2> ]");
-    println!(" 1 [ <p2-a1> ] [ <p2-b1> ] [ <p2-c1> ]");
-    println!("Du hast folgende Würfel:");
-    println!("   [ -- A -- ] [ -- B -- ] [ -- C -- ]");
-    println!(" 1 [ <p1-a1> ] [ <p1-b1> ] [ <p1-c1> ]");
-    println!(" 2 [ <p1-a2> ] [ <p1-b2> ] [ <p1-c2> ]");
-    println!(" 3 [ <p1-a3> ] [ <p1-b3> ] [ <p1-c3> ]");
-    println!("Wo willst du deinen Würfel platzieren?");*/
+    //spielbrett
+    
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Fehler beim Lesen der Eingabe");
+        println!("Astragaloi");
+        println!("##########");
+        println!("Du hast gewürfelt: <dice>");
+        println!("Dein Gegner hat folgende Würfel:");
+        println!("   [ -- A -- ] [ -- B -- ] [ -- C -- ]");
+        println!(" 3 [ <p2-a3> ] [ <p2-b3> ] [ <p2-c3> ]");
+        println!(" 2 [ <p2-a2> ] [ <p2-b2> ] [ <p2-c2> ]");
+        println!(" 1 [ <p2-a1> ] [ <p2-b1> ] [ <p2-c1> ]");
+        println!("Du hast folgende Würfel:");
+        println!("   [ -- A -- ] [ -- B -- ] [ -- C -- ]");
+        println!(" 1 [ <p1-a1> ] [ <p1-b1> ] [ <p1-c1> ]");
+        println!(" 2 [ <p1-a2> ] [ <p1-b2> ] [ <p1-c2> ]");
+        println!(" 3 [ <p1-a3> ] [ <p1-b3> ] [ <p1-c3> ]");
+        println!("Wo willst du deinen Würfel platzieren?");
+        if input == "x" {
+            break()
+        }
+    }
 }
