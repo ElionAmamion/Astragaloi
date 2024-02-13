7198e7109152b2d2ae67ef81107430259c790995
Elion <151955833+ElionAmamion@users.noreply.github.com>
2024-02-13 12:55:08 +0100

    Updated punkte.py
    Hopefully fixed problem about incomplete file

--- main/punkte.py
+++ main/punkte.py
@@ -1,29 +1,29 @@
-from board import get_x, get_y
+import board
 
 def punkte_x():
-    xl = get_x()["xol"] + get_x()["xml"] + get_x()["xul"]
-    xm = get_x()["xom"] + get_x()["xmm"] + get_x()["xum"]
-    xr = get_x()["xor"] + get_x()["xmr"] + get_x()["xur"]
+    xl = x["xol"] + x["xml"] + x["xul"]
+    xm = x["xom"] + x["xmm"] + x["xum"]
+    xr = x["xor"] + x["xmr"] + x["xur"]
 
     return xl + xm + xr
 
 def punkte_y():
-    yl = get_y()["yol"] + get_y()["yml"] + get_y()["yul"]
-    ym = get_y()["yom"] + get_y()["ymm"] + get_y()["yum"]
-    yr = get_y()["yor"] + get_y()["ymr"] + get_y()["yur"]
+    yl = y["yol"] + y["yml"] + y["yul"]
+    ym = y["yom"] + y["ymm"] + y["yum"]
+    yr = y["yor"] + y["ymr"] + y["yur"]
 
     return yl + ym + yr
 
 def x_lmr():
-    xl = get_x()["xol"] + get_x()["xml"] + get_x()["xul"]
-    xm = get_x()["xom"] + get_x()["xmm"] + get_x()["xum"]
-    xr = get_x()["xor"] + get_x()["xmr"] + get_x()["xur"]
+    xl = x["xol"] + x["xml"] + x)["xul"]
+    xm = x["xom"] + x["xmm"] + x["xum"]
+    xr = x["xor"] + x["xmr"] + x["xur"]
 
     return (xl, xm, xr)
 
 def y_lmr():
-    yl = get_y()["yol"] + get_y()["yml"] + get_y()["yul"]
-    ym = get_y()["yom"] + get_y()["ymm"] + get_y()["yum"]
-    yr = get_y()["yor"] + get_y()["ymr"] + get_y()["yur"]
+    yl = y["yol"] + y["yml"] + y["yul"]
+    ym = y["yom"] + y["ymm"] + y["yum"]
+    yr = y["yor"] + y["ymr"] + y["yur"]
 
     return (yl, ym, yr)
